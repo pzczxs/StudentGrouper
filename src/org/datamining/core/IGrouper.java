cdd264980f0f647b634b811a93cc13b65bc22c07
XU, Shuo <pzczxs@gmail.com>
2018-05-23 09:18:37 +0800

    commit for the first time

---
+++ src/org/datamining/core/IGrouper.java
@@ -0,0 +1,18 @@
+package org.datamining.core;
+
+import java.util.List;
+
+/**
+ * 
+ * @author <a href = "mailto:pzczxs@gmail.com">XU, Shuo</a>
+ * version 1.0
+ *
+ */
+public interface IGrouper {
+	
+	public static long SEED_DEFAULT = 56567651; 
+	
+	public static int NFOLD_DEFAULT = 5; 
+	
+	public List<Student> getGroup(final int split);
+}
---
+++ src/org/datamining/core/Student.java
@@ -0,0 +1,62 @@
+package org.datamining.core;
+
+public class Student {
+	private String id; 
+	private String name; 
+	private boolean gender; // true: 男; false: 女
+	private String email; 
+	
+	public Student(final String id, final String name, final boolean gender, final String email) {
+		this.id = id.trim(); 
+		this.name = name.trim(); 
+		this.gender = gender; 
+		this.email = email.trim(); 
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(final String id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(final String name) {
+		this.name = name;
+	}
+
+	public boolean isGender() {
+		return gender;
+	}
+
+	public void setGender(final boolean gender) {
+		this.gender = gender;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(final String email) {
+		this.email = email;
+	}
+
+	@Override
+	public String toString() {
+		StringBuilder sb = new StringBuilder(); 
+		sb.append(id + "\t")
+		  .append("[[" + email + "|")
+		  .append(name + "]]"); 
+		if (gender) {
+			sb.append("男"); 
+		} else {
+			sb.append("女"); 
+		}
+		
+		return sb.toString();
+	}
+}
